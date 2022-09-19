fae0b4ebaccf0ae761ca6e5e54ab7ece0076f808
xiaoxunyao <huchengyea@163.com>
2022-09-19 11:44:17 +0800

    chore: 增加条文，删除Result改为 restful-return

--- project/basic/src/main/java/com/beneway/basic/common/utils/sys_files/FileTypeEnum.java
+++ project/basic/src/main/java/com/beneway/basic/common/utils/sys_files/FileTypeEnum.java
@@ -1,3 +1,17 @@
+/**
+ * 版权所有 2022 - 至今 为 杭州融慧数据科技有限公司 所有
+ *
+ * 版权说明使用 GNU Lesser General Public License v3.0 or later 许可证;
+ * 除非遵守许可说明，否则您无权使用此文件。
+ * 您可以在以下网址获取许可证的副本
+ *          https://spdx.org/licenses/LGPL-3.0-or-later.html
+ * 除非适用法律要求或书面同意，否则软件
+ * 根据许可分发是在“原样”基础上分发的，
+ * 不提供任何明示或暗示的保证或条件。
+ * 请参阅许可证以了解特定语言的管理权限和
+ * 许可证下的限制。
+ */
+
 package com.beneway.basic.common.utils.sys_files;
 
 import java.util.Arrays;
@@ -60,7 +74,7 @@ public enum FileTypeEnum {
     }
 
     public static boolean isType(FileTypeEnum fileTypeEnum, String suffix){
-        return Arrays.stream(fileTypeEnum.suffixs).anyMatch(s -> s.equals(suffix));
+        return Arrays.asList(fileTypeEnum.suffixs).contains(suffix);
     }
 
     public static boolean isImage(String suffix){
@@ -78,7 +92,7 @@ public enum FileTypeEnum {
     public static FileTypeEnum getTypeBySuffix (String suffix) {
         FileTypeEnum[] enums = values();
         for (FileTypeEnum typeEnum : enums) {
-            boolean b = Arrays.stream(typeEnum.suffixs).anyMatch(s -> s.equals(suffix));
+            boolean b = Arrays.asList(typeEnum.suffixs).contains(suffix);
             if (b) {
                 return typeEnum;
             }
