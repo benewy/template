cf67b5a807377788dfa6510abb14fb400b63761d
xiaoxunyao <huchengyea@163.com>
2022-09-22 17:21:47 +0800

    feat: 增加版权信息

--- project/basic/src/main/java/com/beneway/basic/config/MyBatisPlusConfig.java
+++ project/basic/src/main/java/com/beneway/basic/config/MyBatisPlusConfig.java
@@ -1,5 +1,18 @@
-package com.beneway.basic.config;
+/**
+ * 版权所有 2022 - 至今 为 杭州融惠数据科技有限公司 所有
+ *
+ * 版权说明使用 GNU Lesser General Public License v3.0 or later 许可证;
+ * 除非遵守许可说明，否则您无权使用此文件。
+ * 您可以在以下网址获取许可证的副本
+ *          https://spdx.org/licenses/LGPL-3.0-or-later.html
+ * 除非适用法律要求或书面同意，否则软件
+ * 根据许可分发是在“原样”基础上分发的，
+ * 不提供任何明示或暗示的保证或条件。
+ * 请参阅许可证以了解特定语言的管理权限和
+ * 许可证下的限制。
+ */
 
+package com.beneway.basic.config;
 
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
@@ -9,7 +22,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MyBatisPlusConfig {
-  
+
   /**
    * 新的分页插件,一缓和二缓遵循mybatis的规则,需要设置 MybatisConfiguration#useDeprecatedExecutor = false 避免缓存出现问题(该属性会在旧插件移除后一同移除)
    */
--- project/basic/src/main/java/com/beneway/basic/system/sys_filter_unit/dao/SysFilterUnitDao.java
+++ project/basic/src/main/java/com/beneway/basic/system/sys_filter_unit/dao/SysFilterUnitDao.java
@@ -1,3 +1,17 @@
+/**
+ * 版权所有 2022 - 至今 为 杭州融惠数据科技有限公司 所有
+ *
+ * 版权说明使用 GNU Lesser General Public License v3.0 or later 许可证;
+ * 除非遵守许可说明，否则您无权使用此文件。
+ * 您可以在以下网址获取许可证的副本
+ *          https://spdx.org/licenses/LGPL-3.0-or-later.html
+ * 除非适用法律要求或书面同意，否则软件
+ * 根据许可分发是在“原样”基础上分发的，
+ * 不提供任何明示或暗示的保证或条件。
+ * 请参阅许可证以了解特定语言的管理权限和
+ * 许可证下的限制。
+ */
+
 package com.beneway.basic.system.sys_filter_unit.dao;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
