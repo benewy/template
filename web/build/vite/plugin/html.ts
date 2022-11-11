6dbc8895125cd39a5ea8ebd89a39ba4afc06f46b
elonehoo <huchengyea@163.com>
2022-11-11 23:08:58 +0800

    fix: 更新到最新版本并且做了迁移

--- web/build/vite/plugin/html.ts
+++ web/build/vite/plugin/html.ts
@@ -2,14 +2,13 @@
  * Plugin to minimize and use ejs template syntax in index.html.
  * https://github.com/anncwb/vite-plugin-html
  */
-import type { Plugin } from 'vite';
-
-import html from 'vite-plugin-html';
+import {PluginOption} from "vite";
+import { createHtmlPlugin } from 'vite-plugin-html'
 
 import pkg from '../../../package.json';
 import { GLOB_CONFIG_FILE_NAME } from '../../constant';
 
-export function configHtmlPlugin(env: ViteEnv, isBuild: boolean) {
+export function configHtmlPlugin(env: ViteEnv, isBuild: boolean):PluginOption {
   const { VITE_GLOB_APP_TITLE, VITE_PUBLIC_PATH } = env;
 
   const path = VITE_PUBLIC_PATH.endsWith('/') ? VITE_PUBLIC_PATH : `${VITE_PUBLIC_PATH}/`;
@@ -18,23 +17,23 @@ export function configHtmlPlugin(env: ViteEnv, isBuild: boolean) {
     return `${path || '/'}${GLOB_CONFIG_FILE_NAME}?v=${pkg.version}-${new Date().getTime()}`;
   };
 
-  const htmlPlugin: Plugin[] = html({
+  const htmlPlugin = createHtmlPlugin({
     minify: isBuild,
     inject: {
       // Inject data into ejs template
-      injectData: {
+      data: {
         title: VITE_GLOB_APP_TITLE,
       },
       // Embed the generated app.config.js file
       tags: isBuild
         ? [
-            {
-              tag: 'script',
-              attrs: {
-                src: getAppConfigSrc(),
-              },
+          {
+            tag: 'script',
+            attrs: {
+              src: getAppConfigSrc(),
             },
-          ]
+          },
+        ]
         : [],
     },
   });
